c13df4f2bb3a817b12a397d1c270565cccb11253
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2026-06-03 20:21:44 +0600

    nav bar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
-        <header className="px- py-3 border-b-2 border-gray">
-            <h2 className="text-lg font-semibold">🤑 Dev Story</h2>
+        <header className="border-b-0 border-gray flex justify-between px-22 py-5 pt-6">
+            <Link className="text-lg font-semibold" href="/">🤑 Dev Story</Link>
+            <nav className='space-x-5'>
+                <Link href="/about">About</Link>
+                <Link href="/tutorials">Tutorials</Link>
+                <Link href="/about">About</Link>
+                <Link href="/about/contact">Contact</Link>
+                <Link href="/about/team">Teams</Link>
+                <Link href="/dashboard" className='btn rounded-full bg-amber-300 py-5 px-12'>Create Story</Link>
+            </nav>
         </header>
     );
 };
